258333482496b3cae04767fb8d5e56e5e0d8aa4a
Juan17Informatico <juancampuzano2356@gmail.com>
2024-04-30 20:51:43 -0500

    Fin de section 14 - Manejo del estado de los errores al mostrar al usuario

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -12,6 +12,9 @@ export const SearchPage = () => {
 
     const heroes = getHeroesByName(q);
 
+    const showSearch = (q.length === 0); 
+    const showError = (q.length > 0) && heroes.length === 0
+
     const { searchText, onInputChange } = useForm({
         searchText: q
     });
@@ -52,12 +55,19 @@ export const SearchPage = () => {
                 <div className="col-7">
                     <h4>Results</h4>
                     <hr />
+                    {
+                        // (q === '')
+                        // ? <div className="alert alert-primary">Search a hero</div>
+                        // : (heroes.length === 0) && <div className="alert alert-danger">No hero with <b>{q}</b></div>
+                    }
+                    <div className="alert alert-primary animate__animated animate__fadeInLeft" style={{display: showSearch ? '' : 'none'}}>
+                        Search a hero
+                    </div>
 
-                    <div className="alert alert-primary">Search a hero</div>
-
-                    <div className="alert alert-danger">
+                    <div className="alert alert-danger animate__animated animate__fadeInLeft" style={{ display: showError ? '' : 'none' }}>
                         No hero with <b>{q}</b>
                     </div>
+
                     {
                       heroes.map((hero) => (
                           <HeroCard key={hero.id} {...hero} />
